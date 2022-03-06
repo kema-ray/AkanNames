aeb2098e7e8cccc08f0cfae8d87656322eb43fc8
kema-ray <rachelkemuma99@gmail.com>
2022-03-06 21:54:02 +0300

    add formula to calculate days

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ akanForm.addEventListener("submit", (e) => {
     let yearOfBirth = dateOfBirth.getFullYear();
     let birthMonth = dateOfBirth.getMonth() + 1;
     let dayOfBirth = dateOfBirth.getDate();
+    
+    CC = yearOfBirth.toString().substr(0, 2);
+    YY = yearOfBirth.toString().substr(2, 2);
+    MM = birthMonth
+    DD = dayOfBirth
+
+    dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
+    dayOfTheWeek = parseInt(dayOfTheWeek);
 }
